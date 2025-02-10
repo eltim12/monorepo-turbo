239456a27429c493fe2e788fed6ee249f634aa1e
eltim12 <eltim2000@gmail.com>
2025-02-10 10:50:09 +0700

    feat: remove comments

--- apps/frontend/store/store.ts
+++ apps/frontend/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authSlice"; // Import the authentication reducer
+import authReducer from "./authSlice";
 
 export const store = configureStore({
   reducer: {
